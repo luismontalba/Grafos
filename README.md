5d1dd61de5bcbb9928b3e4ebd29e1d3c2a29f9b3
luismontalba <luismaront@gmail.com>
2025-01-24 17:42:27 +0100

    Alert impossibe dates an edges

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Features:
 * Nodes have details: description and date
 * Nodes always ordered by date
 * Today date assigned when creating new node
-* Impossible edges highlighted in red
+* Impossible edges and dates highlighted in red
 * Tags filter to search nodes in view
 * Button to update past dates to today dates
 * Earlies dates calculated depending on duration of nodes
--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', init);
 
+// starting
 function init() {
   loadFromLocalStorage();
-  sortNodesByDate();
   fixCanvas();
   fixDraw();
-  updateTagsFilter();
+  updateView();
 }
 
 function fixDraw() {
@@ -16,6 +16,7 @@ function fixDraw() {
 	debouncedSaveToLocalStorage();
 }
 
+// Auxiliar tools
 // Debuncing function
 const debounce = (func, wait) => {
   let timeout;
@@ -55,7 +56,7 @@ const today = new Date();
 const getTodayDate = () => formatDate(today);
 const getTodayLongDate = () => formatLongDate(today);
 
-
+// Building the view
 // HTML references
 const container = document.getElementById('mynetwork');
 const filterContainer = document.getElementById('filterContainer');
@@ -76,11 +77,11 @@ const importButton = document.getElementById('importButton');
 
 // Creating a network
 const nodes = new vis.DataSet([
-  {id: 1, label: 'Node 1\n2025-01-01', date: '2025-01-01', description: 'Descripción del Nodo 1', tags: ['tag1']},
-  {id: 2, label: 'Node 2\n2025-03-01', date: '2025-03-01', time: '2', description: 'Descripción del Nodo 2', tags: ['tag2']},
-  {id: 3, label: 'Node 3\n2025-03-01', date: '2025-03-01', description: 'Descripción del Nodo 3', tags: ['tag3']},
-  {id: 4, label: 'Node 4\n2025-03-01', date: '2025-03-01', time: '15', description: 'Descripción del Nodo 4', tags: ['tag4']},
-  {id: 5, label: 'Node 5\n2025-05-01', date: '2025-05-01', description: 'Descripción del Nodo 5', tags: ['tag5']}
+  {id: 1, label: 'Node 1\n2025-01-01', date: '2025-01-01', cdate: '', time: '0',description: 'Descripción del Nodo 1', tags: ['tag1']},
+  {id: 2, label: 'Node 2\n2025-02-01', date: '2025-02-01', cdate: '', time: '40', description: 'Descripción del Nodo 2', tags: ['tag2']},
+  {id: 3, label: 'Node 3\n2025-03-01', date: '2025-03-01', cdate: '', time: '0', description: 'Descripción del Nodo 3', tags: ['tag3']},
+  {id: 4, label: 'Node 4\n2025-04-01', date: '2025-04-01', cdate: '', time: '10', description: 'Descripción del Nodo 4', tags: ['tag4']},
+  {id: 5, label: 'Node 5\n2025-05-01', date: '2025-05-01', cdate: '', time: '0', description: 'Descripción del Nodo 5', tags: ['tag5']}
 ]);
 
 const edges = new vis.DataSet([
@@ -94,27 +95,30 @@ const edges = new vis.DataSet([
 const data = { nodes, edges };
 const options = {
   interaction: {
-    hover: true,
+    hover: false,
     multiselect: true,
-    dragView: true
+    dragView: true,
+    selectable: true,
+    selectConnectedEdges: false,
   },
   manipulation: {
     enabled: true,
     addNode: function (data, callback) {
       data.label = `Nuevo Nodo\n${getTodayDate()}`;
       data.date = getTodayDate();
+      data.cdate = '';
+      data.time = '0';
       data.description = '';
       data.tags = [];
       callback(data);
-      sortNodesByDate();
+      updateView();
       debouncedSaveToLocalStorage();
     },
     addEdge: function (data, callback) {
       if (data.from !== data.to) {
         data.arrows = 'to';
         callback(data);
-        sortNodesByDate();
-        alertEdges();
+        updateView();
         debouncedSaveToLocalStorage();
       }
     }
@@ -140,6 +144,15 @@ function fixCanvas() {
   canvas.setAttribute('height', '10000');
   const ctx = canvas.getContext("2d");
   ctx.scale(2, 2);
+}
+
+// Updating the view
+function updateView() {
+  sortNodesByDate();
+  alertEdges();
+  dateCalc();
+  updateTagsFilter();
+  network. redraw();
 }
 
 // Function sorting nodes by date
@@ -170,17 +183,6 @@ function sortNodesByDate() {
   });
 }
 
-// Event saving node position
-network.on("dragEnd", function (params) {
-  params.nodes.forEach(nodeId => {
-    const position = network.getPositions([nodeId])[nodeId];
-    nodes.update({ id: nodeId, x: position.x, y: position.y });
-  });
-  sortNodesByDate();
-  alertEdges();
-  debouncedSaveToLocalStorage();
-});
-
 // Function alerting impossible edges going from future to past
 function alertEdges() {
   const allNodes = nodes.get();
@@ -197,104 +199,39 @@ function alertEdges() {
   });
 }
 
-// Event displaying node details
-let selectedNodeId = null;
-network.on("click", function (params) {
-  if (params.nodes.length === 1) {
-    selectedNodeId = params.nodes[0];
-    const node = nodes.get(selectedNodeId);
-    detailContainer.style.display = 'block';
-    labelInput.value = node.label.split('\n')[0];
-    dateInput.value = node.date;
-    cdateInput.value = node.cdate;
-    timeInput.value = node.time;
-    descriptionInput.value = node.description;
-    updateTagsList(node.tags);
-  } else {
-    detailContainer.style.display = 'none';
-  }
-});
-
 // Function calculating date
 function dateCalc() {
   const allNodes = nodes.get();
   allNodes.forEach(node => { 
     const parentsIds = network.getConnectedNodes(node.id, 'from');
     const parentsNodes = nodes.get(parentsIds);
-    const datesFromParents = parentsNodes.map(parent => addDays(new Date(parent.date), Number(parent.time)));
-    const newCdate = formatDate(new Date(Math.max.apply(null,datesFromParents)));
-    nodes.update({id: node.id, cdate: newCdate});
-  });
-}
-
-// Event saving node details
-saveDetailButton.addEventListener('click', function() {
-  if (selectedNodeId !== null) {
-    const newLabel = labelInput.value;
-    const newDate = dateInput.value;
-    const newTime = timeInput.value
-    const newDescription = descriptionInput.value;
-    const newTags = getTagsFromUI();
-    nodes.update({
-      id: selectedNodeId,
-      label: `${newLabel}\n${newDate}`,
-      date: newDate,
-      time: newTime,
-      description: newDescription,
-      tags: newTags
-    });
-    detailContainer.style.display = 'none';
-    sortNodesByDate();
-    alertEdges();
-    updateTagsFilter();
-    dateCalc();
-    debouncedSaveToLocalStorage();
-  }
-});
-
-// Managing tags in node details
-addTagButton.addEventListener('click', function() {
-  if (selectedNodeId !== null) {
-    const tag = tagInput.value.trim();
-    if (tag) {
-      const node = nodes.get(selectedNodeId);
-      if (!node.tags.includes(tag)) {
-        node.tags.push(tag);
-        updateTagsList(node.tags);
-        tagInput.value = '';
-      }
+    if (parentsNodes.length > 0) {
+      const datesFromParents = parentsNodes.map(parent => addDays(new Date(parent.date), Number(node.time)));
+      const newCdate = formatDate(new Date(Math.max.apply(null, datesFromParents)));
+      nodes.update({
+        id: node.id,
+        cdate: newCdate,
+        font: { color: new Date(node.date) < new Date(node.cdate) ? "red" : "#343434" }
+      });
+    } else {
+      nodes.update({
+        id: node.id,
+        cdate: node.date,
+        font: { color: "#343434" }
+      });
     }
-  }
-});
-
-function updateTagsList(tags) {
-  tagsList.innerHTML = '';
-  tags.forEach(tag => {
-    const tagElement = document.createElement('span');
-    tagElement.className = 'tag';
-    tagElement.textContent = tag;
-    const deleteButton = document.createElement('span');
-    deleteButton.className = 'delete-tag';
-    deleteButton.textContent = 'x';
-    deleteButton.addEventListener('click', function() {
-      const node = nodes.get(selectedNodeId);
-      node.tags = node.tags.filter(t => t !== tag);
-      updateTagsList(node.tags);
+    const criticalParents = parentsNodes.filter(parent => parent.date === formatDate(addDays(new Date(node.cdate), -Number(node.time))));
+    const parentsEdgesIds = network.getConnectedEdges(node.id);
+    const parentsEdges = edges.get(parentsEdgesIds);
+    parentsEdges.forEach(edge => {
+      edges.update({
+        id: edge.id,
+        color: { color: criticalParents.map(parent => parent.id).includes(edge.from) && new Date(node.date) < new Date(node.cdate) ? "red" : "blue" }
+      });
     });
-    tagElement.appendChild(deleteButton);
-    tagsList.appendChild(tagElement);
   });
 }
 
-function getTagsFromUI() {
-  const tags = [];
-  tagsList.querySelectorAll('.tag').forEach(tagElement => {
-    tags.push(tagElement.textContent.slice(0, -1));
-  });
-  return tags;
-}
-
-// Displaying box with tags to be filtered
 let tagsChecked = [];
 function updateTagsFilter() {
   filterTagsContainer.innerHTML = '';
@@ -325,6 +262,18 @@ function updateTagsFilter() {
   });
 }
 
+// Working with network
+// Event saving node position
+network.on("dragEnd", function (params) {
+  params.nodes.forEach(nodeId => {
+    const position = network.getPositions([nodeId])[nodeId];
+    nodes.update({ id: nodeId, x: position.x, y: position.y });
+  });
+  updateView();
+  debouncedSaveToLocalStorage();
+});
+
+// Displaying box with tags to be filtered
 // Event filtering nodes by selected tags
 filterTagsContainer.addEventListener("change", function(event) {
   const allNodes = nodes.get();
@@ -357,11 +306,93 @@ updateToday.addEventListener('click', function(event) {
       });
     }
   })
-  sortNodesByDate();
-  alertEdges();
+  updateView();
   debouncedSaveToLocalStorage();
 });
 
+// Woking with details
+// Event displaying node details
+let selectedNodeId = null;
+network.on("click", function (params) {
+  if (params.nodes.length === 1) {
+    selectedNodeId = params.nodes[0];
+    const node = nodes.get(selectedNodeId);
+    detailContainer.style.display = 'block';
+    labelInput.value = node.label.split('\n')[0];
+    dateInput.value = node.date;
+    dateInput.style.color = new Date(node.date) < new Date(node.cdate) ? 'red' : 'black';
+    cdateInput.value = node.cdate;
+    timeInput.value = node.time;
+    descriptionInput.value = node.description;
+    updateTagsList(node.tags);
+  } else {
+    detailContainer.style.display = 'none';
+  }
+});
+
+// Managing tags in node details
+addTagButton.addEventListener('click', function() {
+  if (selectedNodeId !== null) {
+    const tag = tagInput.value.trim();
+    if (tag) {
+      const node = nodes.get(selectedNodeId);
+      if (!node.tags.includes(tag)) {
+        node.tags.push(tag);
+        updateTagsList(node.tags);
+        tagInput.value = '';
+      }
+    }
+  }
+});
+function updateTagsList(tags) {
+  tagsList.innerHTML = '';
+  tags.forEach(tag => {
+    const tagElement = document.createElement('span');
+    tagElement.className = 'tag';
+    tagElement.textContent = tag;
+    const deleteButton = document.createElement('span');
+    deleteButton.className = 'delete-tag';
+    deleteButton.textContent = 'x';
+    deleteButton.addEventListener('click', function() {
+      const node = nodes.get(selectedNodeId);
+      node.tags = node.tags.filter(t => t !== tag);
+      updateTagsList(node.tags);
+    });
+    tagElement.appendChild(deleteButton);
+    tagsList.appendChild(tagElement);
+  });
+}
+function getTagsFromUI() {
+  const tags = [];
+  tagsList.querySelectorAll('.tag').forEach(tagElement => {
+    tags.push(tagElement.textContent.slice(0, -1));
+  });
+  return tags;
+}
+
+// Event saving node details
+saveDetailButton.addEventListener('click', function() {
+  if (selectedNodeId !== null) {
+    const newLabel = labelInput.value;
+    const newDate = dateInput.value;
+    const newTime = timeInput.value
+    const newDescription = descriptionInput.value;
+    const newTags = getTagsFromUI();
+    nodes.update({
+      id: selectedNodeId,
+      label: `${newLabel}\n${newDate}`,
+      date: newDate,
+      time: newTime,
+      description: newDescription,
+      tags: newTags
+    });
+    detailContainer.style.display = 'none';
+    updateView();
+    debouncedSaveToLocalStorage();
+  }
+});
+
+// Managing persistence
 // Loading from local Storage
 function loadFromLocalStorage() {
   const storedData = localStorage.getItem('graphData');
@@ -413,8 +444,7 @@ importButton.addEventListener('change', function(event) {
     nodes.add(graphData.nodes);
     edges.clear();
     edges.add(graphData.edges);
-    sortNodesByDate();
-    alertEdges();
+    updateView();
     debouncedSaveToLocalStorage();
   };
   reader.readAsText(file);
